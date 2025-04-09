92f3b3263a58a2cf930ea90d7a169db754a807c6
Panos Tsavdaris <panagiotis.tsavdaris@oaknorth.co.uk>
2025-04-09 14:26:52 +0100

    Idiomatic Go & testify (#2)

--- credit_score.go
+++ credit_score.go
@@ -1,12 +1,15 @@
 package creditscore
 
-import "time"
+import (
+	"math"
+	"time"
+)
 
-type InvoiceStatus string
+type InvoiceStatus int
 
 const (
-	Paid   InvoiceStatus = "PAID"
-	Unpaid InvoiceStatus = "UNPAID"
+	Paid InvoiceStatus = iota
+	Unpaid
 )
 
 type Invoice struct {
@@ -15,18 +18,18 @@ type Invoice struct {
 }
 
 type CreditReport struct {
-	PaymentHistory              []Invoice
+	PaymentHistory              []*Invoice
 	CreditUtilisationPercentage float64
 }
 
-type CreditScoreCategory string
+type CreditScoreCategory int
 
 const (
-	Fair      CreditScoreCategory = "fair"
-	Good      CreditScoreCategory = "good"
-	Excellent CreditScoreCategory = "excellent"
-	Poor      CreditScoreCategory = "poor"
-	VeryPoor  CreditScoreCategory = "very poor"
+	Fair CreditScoreCategory = iota
+	Good
+	Excellent
+	Poor
+	VeryPoor
 )
 
 type CreditScore struct {
@@ -34,32 +37,32 @@ type CreditScore struct {
 	Category CreditScoreCategory
 }
 
-func GetCreditScore(c CreditReport) CreditScore {
+func GetCreditScore(c *CreditReport) *CreditScore {
 	x := c.CreditUtilisationPercentage
 
 	switch {
 	case x > 0.9:
-		return CreditScore{
+		return &CreditScore{
 			Value:    560,
 			Category: VeryPoor,
 		}
 	case x > 0.7:
-		return CreditScore{
+		return &CreditScore{
 			Value:    720,
 			Category: Poor,
 		}
 	case x > 0.5:
-		return CreditScore{
+		return &CreditScore{
 			Value:    880,
 			Category: Fair,
 		}
 	case x > 0.3:
-		return CreditScore{
+		return &CreditScore{
 			Value:    960,
 			Category: Good,
 		}
 	default:
-		return CreditScore{
+		return &CreditScore{
 			Value:    999,
 			Category: Excellent,
 		}
@@ -67,5 +70,5 @@ func GetCreditScore(c CreditReport) CreditScore {
 }
 
 func CalculatePercentage(value, total float64) float64 {
-	return float64(int((value/total)*100)) / 100
+	return math.Round(value/total*100) / 100
 }
